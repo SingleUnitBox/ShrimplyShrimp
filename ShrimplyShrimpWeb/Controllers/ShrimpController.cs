cc74602cc760bea63f62d95ee5142528102ed10b
SingleUnitBox <cezaryzukowski90@gmail.com>
2023-03-12 22:30:02 +0000

    add Create Shrimp

--- ShrimplyShrimpWeb/Controllers/ShrimpController.cs
+++ ShrimplyShrimpWeb/Controllers/ShrimpController.cs
@@ -1,12 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShrimplyShrimpWeb.Data;
+using ShrimplyShrimpWeb.Models;
 
 namespace ShrimplyShrimpWeb.Controllers
 {
     public class ShrimpController : Controller
     {
+        private readonly ShrimplyShrimpDbContext _shrimplyShrimpDbContext;
+
+        public ShrimpController(ShrimplyShrimpDbContext shrimplyShrimpDbContext)
+        {
+            _shrimplyShrimpDbContext = shrimplyShrimpDbContext;
+        }
         public IActionResult Index()
         {
+            IEnumerable<Shrimp> shrimps = _shrimplyShrimpDbContext.Shrimps;
+            return View(shrimps);
+        }
+        //GET
+        public IActionResult Create()
+        {
             return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Shrimp shrimp)
+        {
+            if (shrimp.Name == shrimp.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("ShrimpError", "Shrimp Name cannot match DisplayOrder");
+            }
+            if (ModelState.IsValid)
+            {
+                _shrimplyShrimpDbContext.Shrimps.Add(shrimp);
+                _shrimplyShrimpDbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(shrimp);
         }
     }
 }
--- ShrimplyShrimpWeb/Models/Shrimp.cs
+++ ShrimplyShrimpWeb/Models/Shrimp.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ShrimplyShrimpWeb.Models
 {
@@ -8,7 +9,7 @@ namespace ShrimplyShrimpWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
     }
